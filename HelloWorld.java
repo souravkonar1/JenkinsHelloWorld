eab9675c1989d5f322d10e2e838cef45ca904f34
souravkonar1 <intrasourav@gmail.com>
2021-12-30 12:00:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("....*****Hello World & Welcome to Jenkins Training****....");
+		//System.out.println("....*****Hello World & Welcome to Jenkins Training****....");
+		System.out.println("....*****Made some changes****....");
 	}
 }
